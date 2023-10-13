9393256809a5a970d0a0d68b65302750f540ec7b
saurav anand <saurav.anand1@justdial.com>
2023-10-13 19:43:41 +0530

    todo design updated

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,9 @@ import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CopyRight from '../components/footerTodo';
 import FooterTodo from '../components/footerTodo';
+import { FormControl, InputAdornment, InputLabel, OutlinedInput, TextField } from '@mui/material';
+import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 
 const cards = [1, 2, 3, 4, 5, 6];
 
@@ -36,56 +39,25 @@ const Index = () => {
         </Toolbar>
       </AppBar>
       <main>
-        {/* Hero unit */}
-        <Box
-          sx={{
-            bgcolor: 'background.paper',
-            pt: 8,
-            pb: 6,
-          }}
-        >
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-              gutterBottom
-            >
-              Album layout
-            </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Something short and leading about the collection below—its contents,
-              the creator, etc. Make it short and sweet, but not too short so folks
-              don&apos;t simply skip over it entirely.
-            </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button variant="contained">Main call to action</Button>
-              <Button variant="outlined">Secondary action</Button>
-            </Stack>
-          </Container>
-        </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          {/* End hero unit */}
+        <Container sx={{ py: 8 }} maxWidth="lg">
+          <TextField fullWidth id="outlined-basic" label="Task" variant="outlined"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AssignmentOutlinedIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Stack sx={{ pt: 4, pb: 8 }} direction="row" justifyContent="center">
+            <Button variant="contained"><AddCircleOutlineOutlinedIcon />&nbsp;Add Todo</Button>
+          </Stack>
           <Grid container spacing={4}>
             {cards.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card
                   sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
                 >
-                  <CardMedia
-                    component="div"
-                    sx={{
-                      // 16:9
-                      pt: '56.25%',
-                    }}
-                    image="https://source.unsplash.com/random?wallpapers"
-                  />
                   <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h5" component="h2">
                       Heading
